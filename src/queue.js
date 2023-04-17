645a8428d9634482d28264d4e0a50232e446d3a1
Zhiznevski <w.heisenberggg@gmail.com>
2023-04-17 23:11:22 +0300

    feat: add queue

--- src/queue.js
+++ src/queue.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 // const { ListNode } = require('../extensions/list-node.js');
 
@@ -14,23 +14,40 @@ const { NotImplementedError } = require('../extensions/index.js');
  * queue.getUnderlyingList() // returns { value: 3, next: null }
  */
 class Queue {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+  }
 
   getUnderlyingList() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this;
   }
-
-  enqueue(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  enqueue(value) {
+    const tail = this.tail;
+    const element = { next: null, value };
+    if (tail) {
+      tail.next = element;
+      this.tail = element;
+    } else {
+      this.head = element;
+      this.tail = element;
+    }
   }
-
   dequeue() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    const element = this.head;
+    if (!element) {
+      return null;
+    }
+    if (this.tail === element) {
+      this.head = null;
+      this.tail = null;
+    } else {
+      this.head = element.next;
+    }
+    return element.value;
   }
 }
 
 module.exports = {
-  Queue
+  Queue,
 };
